48f8fe4b9d14035f2591312d8bfe2735d112676d
shuyuanshi <nauyuhs@gmail.com>
2021-12-26 12:16:59 -0800

    lint fix

--- app/core/models.py
+++ app/core/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('user must have an email address')
-        
-        user = self.model(email = self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -18,16 +18,15 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
-        
+        user.save(using=self._db)
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
-    
     USERNAME_FIELD = 'email'
