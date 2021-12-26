00b0a402691b27b4de9276cc28a8f8d80ee06a8b
shuyuanshi <nauyuhs@gmail.com>
2021-12-26 12:03:32 -0800

    fix tests

--- app/core/models.py
+++ app/core/models.py
@@ -5,10 +5,21 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email = email, **extra_fields)
+        if not email:
+            raise ValueError('user must have an email address')
+        
+        user = self.model(email = self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using = self._db)
 
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
+        
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
